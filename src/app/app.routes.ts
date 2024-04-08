e8384d97ee112ae817f97145aa3b92693d8a383a
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-08 16:01:24 -0500

    creacion de rutas de pokemon y paginas por clasificacion de tipos y habilidades

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pokemon',
+    loadChildren: () => import('./pokemon/pokemon.routes').then(m => m.routes)
+  },
+  {
+    path: '**',
+    redirectTo: 'pokemon'
+  },
+];
---
+++ src/app/pokemon/pokemon.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TiposComponent } from './tipos/tipos.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: TiposComponent
+  },
+];

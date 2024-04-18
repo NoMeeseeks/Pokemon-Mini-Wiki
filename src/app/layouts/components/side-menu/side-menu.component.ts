c217b06d8ec3ca474dc2c2f43d052bb745813060
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-17 22:14:12 -0500

    creando la barra lateral del proyecto

--- src/app/layouts/components/side-menu/side-menu.component.ts
+++ src/app/layouts/components/side-menu/side-menu.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+export interface menuItem {
+  nombre: string;
+  ruta: string;
+}
 
 @Component({
   selector: 'side-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
-
+  public menu: menuItem[] = [
+    {
+      nombre: 'tipos',
+      ruta: ''
+    },
+    {
+      nombre: 'habilidades',
+      ruta: ''
+    }];
 }
